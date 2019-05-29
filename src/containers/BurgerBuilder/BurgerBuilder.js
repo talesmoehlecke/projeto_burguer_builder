c826bf7ffd05d22a5f22b64c73dad8885e6d8944
Tales Moehlecke <tmoehlecke@gvdasa.com.br>
2019-05-29 14:47:09 -0300

    160

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React, {  Component } from 'react';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component{
     
@@ -9,16 +17,30 @@ class BurgerBuilder extends Component{
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
     
     render(){
         return(
             <>
                 <Burger ingredients={this.state.ingredients}/>
-            <div>
-                Ingredients selector
-            </div>
+                <BuildControls />
             </>
         )
     }
